eef0c249e2a6a2337a21963b67df3cdf8e60b578
류형철 <fbgudcjf2@gmail.com>
2025-04-01 13:48:55 +0900

    feat(main) JPA Auditing 어노테이션 추가

--- src/main/java/com/cordingrecipe/scheduledevelop/ScheduleDevelopApplication.java
+++ src/main/java/com/cordingrecipe/scheduledevelop/ScheduleDevelopApplication.java
@@ -2,7 +2,9 @@ package com.cordingrecipe.scheduledevelop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 적용
 @SpringBootApplication
 public class ScheduleDevelopApplication {
 
