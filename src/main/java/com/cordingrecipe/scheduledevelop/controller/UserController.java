dc8864de02dabfd217f3f8450e4fb7fbbaeb7ec2
류형철 <fbgudcjf2@gmail.com>
2025-04-02 01:03:14 +0900

    feat: UserController 에 UserService 의존성 주입

---
+++ src/main/java/com/cordingrecipe/scheduledevelop/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.cordingrecipe.scheduledevelop.controller;
+
+import com.cordingrecipe.scheduledevelop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+}
