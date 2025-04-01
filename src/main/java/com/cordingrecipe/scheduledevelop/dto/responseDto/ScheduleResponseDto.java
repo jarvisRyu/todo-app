1c55dfec2d9223eb5926bed411cea4e61c28eac5
류형철 <fbgudcjf2@gmail.com>
2025-04-01 17:23:57 +0900

    feat(ResponseDto) 응답 dto 추가

---
+++ src/main/java/com/cordingrecipe/scheduledevelop/dto/responseDto/ScheduleResponseDto.java
@@ -0,0 +1,23 @@
+package com.cordingrecipe.scheduledevelop.dto.responseDto;
+
+import com.cordingrecipe.scheduledevelop.entity.Schedule;
+
+import java.time.LocalDateTime;
+
+public class ScheduleResponseDto {
+
+    //생성시 보여지는 dto
+    private Long id;
+    private String username;
+    private String todoTitle;
+    private String todoContents;
+    private LocalDateTime createdAt;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.username = schedule.getUsername();
+        this.todoTitle = schedule.getTodoTitle();
+        this.todoContents = schedule.getTodoContents();
+        this.createdAt = schedule.getCreatedAt();
+    }
+}
