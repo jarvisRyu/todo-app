904db336ca93ef4721db04d60f25c1f80ff3fb15
류형철 <fbgudcjf2@gmail.com>
2025-04-01 11:40:33 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/cordingrecipe/scheduledevelop/ScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordingrecipe.scheduledevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
