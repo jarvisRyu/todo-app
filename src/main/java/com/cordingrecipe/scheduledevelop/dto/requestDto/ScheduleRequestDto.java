4d1883337bd2ee610c0b20cdb893dc6864dabd35
류형철 <fbgudcjf2@gmail.com>
2025-04-01 17:23:45 +0900

    feat(RequestDto) 요청 dto 추가

---
+++ src/main/java/com/cordingrecipe/scheduledevelop/dto/requestDto/ScheduleRequestDto.java
@@ -0,0 +1,19 @@
+package com.cordingrecipe.scheduledevelop.dto.requestDto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+    //일정생성 request dto
+    private String username;
+
+    private String todoTitle;
+
+    private String todoContents;
+
+    public ScheduleRequestDto(String username, String todoTitle, String todoContents) {
+        this.username = username;
+        this.todoTitle = todoTitle;
+        this.todoContents = todoContents;
+    }
+}
