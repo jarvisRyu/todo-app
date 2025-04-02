f7ed17a33ff67a0510fd845a3aa08079afb588ee
류형철 <fbgudcjf2@gmail.com>
2025-04-02 11:08:31 +0900

    feat: User 생성 기능 추가

--- src/main/java/com/cordingrecipe/scheduledevelop/controller/UserController.java
+++ src/main/java/com/cordingrecipe/scheduledevelop/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.cordingrecipe.scheduledevelop.controller;
 
+import com.cordingrecipe.scheduledevelop.dto.requestDto.SignUpRequestDto;
+import com.cordingrecipe.scheduledevelop.dto.responseDto.SignupResponseDto;
 import com.cordingrecipe.scheduledevelop.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    //생성-회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponseDto> signUp(@RequestBody SignUpRequestDto requestDto){
+        SignupResponseDto signupResponseDto = userService.signUp(requestDto);
+        return new ResponseEntity<>(signupResponseDto, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/cordingrecipe/scheduledevelop/service/UserService.java
+++ src/main/java/com/cordingrecipe/scheduledevelop/service/UserService.java
@@ -1,5 +1,8 @@
 package com.cordingrecipe.scheduledevelop.service;
 
+import com.cordingrecipe.scheduledevelop.dto.requestDto.SignUpRequestDto;
+import com.cordingrecipe.scheduledevelop.dto.responseDto.SignupResponseDto;
+import com.cordingrecipe.scheduledevelop.entity.User;
 import com.cordingrecipe.scheduledevelop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public SignupResponseDto signUp(SignUpRequestDto dto){
+        User user =new User(dto.getUsername(),dto.getEmail(),dto.getPassword());
+        User saveUser = userRepository.save(user);
+
+        return new SignupResponseDto(saveUser);
+    }
 }
