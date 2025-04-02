c57e4ef6350ec37bb1f6e23099637cb96843a324
류형철 <fbgudcjf2@gmail.com>
2025-04-02 11:06:11 +0900

    refactor: 2단계 Schedule Entity 수정
    username 필드 -> user 테이블 id JoinColumn 지정

--- src/main/java/com/cordingrecipe/scheduledevelop/entity/Schedule.java
+++ src/main/java/com/cordingrecipe/scheduledevelop/entity/Schedule.java
@@ -11,9 +11,9 @@ public class Schedule extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private String username;
+//
+//    @Column(nullable = false)
+//    private String username;
 
     @Column(nullable = false)
     private String todoTitle;
@@ -21,7 +21,7 @@ public class Schedule extends BaseEntity{
     private String todoContents;
 
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name="user_id") //외래키
     private User user;
 
     public void setUser(User user) {
@@ -30,8 +30,12 @@ public class Schedule extends BaseEntity{
 
     public Schedule(){} //기본생성자
 
-    public Schedule(String username, String todoTitle, String todoContents) {
-        this.username = username;
+//    public Schedule(String username, String todoTitle, String todoContents) {
+//        this.username = username;
+//        this.todoTitle = todoTitle;
+//        this.todoContents = todoContents;
+//    }
+    public Schedule( String todoTitle, String todoContents) {
         this.todoTitle = todoTitle;
         this.todoContents = todoContents;
     }
