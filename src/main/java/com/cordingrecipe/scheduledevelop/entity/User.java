1e0588e51e25547827adff8b2fe75b00296c8c54
류형철 <fbgudcjf2@gmail.com>
2025-04-02 11:07:33 +0900

    feat: User Entity password 추가

--- src/main/java/com/cordingrecipe/scheduledevelop/entity/User.java
+++ src/main/java/com/cordingrecipe/scheduledevelop/entity/User.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Table(name="user")
 public class User extends BaseEntity{
 
-    @Id
+    @Id //PK
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -19,10 +19,16 @@ public class User extends BaseEntity{
     @Column(nullable = false,unique = true)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
     public User(){ } //기본생성자
 
-    public User(String username,String email){
+    public User(String username,String email,String password){
         this.username=username;
         this.email=email;
+        this.password=password;
     }
 }
+//email이 중복된 값이 저장되지못하게 설정
+//1 2 3 4 5
